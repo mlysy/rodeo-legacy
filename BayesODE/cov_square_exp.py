c4882a25e1ee47ef372e552ed80aa390ebb31060
Mohan Wu <mohanwu2014@gmail.com>
2019-04-03 08:41:16 -0400

    added documentation

--- BayesODE/cov_square_exp.py
+++ BayesODE/cov_square_exp.py
@@ -1,21 +1,22 @@
 """
 .. module:: cov_square_exp
-    :synopsis: Covariance and cross-covariance functions for the solution process `x_t` and its derivative `v_t = dx_t/dt` under the squared-exponential correlation model, `cov(v_t, v_s) = exp(-|t-s|^2/gamma^2)`.
+    :synopsis: Covariance and cross-covariance functions for the solution process x_t and its derivative v_t = dx_t/dt under the squared-exponential correlation model, cov(v_t, v_s) = exp(-|t-s|^2/gamma^2).
 """
+
+from numba import jit
+from math import exp, sqrt, pi, erf
 
 @jit
 def cov_vv_se(t, s, gamma):
-    """Calculate covariance function of `v_t`.
+    """Computes the covariance function for the derivative :math:`v_t`. 
 
-    Evaluates `cov(v_t, v_s) = exp(-|t-s|^2/gamma^2)`.
-
-    :param t: First time point.
+    :param t: Time point at t
     :type t: float
-    :param s: Second time point.
+    :param s: Time point at s
     :type s: float
-    :param gamma: Decorrelation time, such that `cov(v_t, v_{t+gamma}` = 1/e`.
+    :param gamma: Decorrelation time, such that :math:`cov(v_t, v_{t+\gamma}) = 1/e`.
     :type gamma: float
-    :returns: The value of the covariance.
+    :returns: Evaluates :math:`cov(v_t, v_s)`.
     :rtype: float
     """
     # no point in dragging around sqrt(pi), etc.
@@ -23,32 +24,51 @@ def cov_vv_se(t, s, gamma):
     # z = (t-s)/gamma
     # return exp(-z*z) # better to use z*z instead of z**2, google it :)
     # I've left your original code for now...
-    return exp(-((t - s)**2) / (4 * gamma**2)) * sqrt(pi) * gamma
+    gamma2 = gamma*gamma
 
+    return exp(-((s - t)**2) / (4 * gamma2)) * sqrt(pi) * gamma
 
 @jit
 def cov_xv_se(t, s, gamma):
-    """Calculate cross-covariance between `x_t` and `v_t`.
+    """Computes the cross-covariance function for the solution process :math:`x_t` and its derivative :math:`v_t`. 
 
-    .. note::
-    - Replace integer powers by multiplication!
-    - Better to break up calculation into multiple lines, and save calculations whenever possible.  For example, you compute `gamma**3` four times in this function...better to save it once as `g3 = gamma**3`.
+    :param t: time point at t
+    :type t: float
+    :param s: time point at s
+    :type s: float
+    :param gamma: Decorrelation time, such that :math:`cov(v_t, v_{t+\gamma}) = 1/e`.
+    :type gamma: float
+    :returns: Evaluates :math:`cov(x_t, v_s)`.
+    :rtype: float
     """
-    ans = pi * gamma**2 * (s) * erf(s / (2 * w)) \
-        + 2 * sqrt(pi) * gamma**3 * exp(-(s**2) / (4 * gamma**2)) \
-        - pi * gamma**2 * (t - s) * erf((t - s) / (2 * gamma)) \
-        - 2 * sqrt(pi) * gamma**3 * exp(-(t - s)**2 / (4 * gamma**2)) \
-        + pi * gamma**2 * s * erf(t / (2 * gamma)) \
-        + 2 * sqrt(pi) * gamma**3 * exp(-(t**2) / (4 * gamma**2)) \
-        - 2 * sqrt(pi) * gamma**3
-    return ans
-    # return integrate.dblquad(sigma_xdot, 0, t, lambda s: 0, lambda s: s)
+    gamma2 = gamma*gamma
 
+    ans = pi * gamma2 * erf((t - s) / (2 * gamma)) + pi * gamma2 * erf(s / (2 * gamma))
+    return ans
 
 @jit
 def cov_xx_se(t, s, gamma):
-    """Calculate covariance function of `x_t`.
+    """Computes the covariance function for the solution process :math:`x_t`. 
+
+    :param t: time point at t
+    :type t: float
+    :param s: time point at s
+    :type s: float
+    :param gamma: Decorrelation time, such that :math:`cov(v_t, v_{t+\gamma}) = 1/e`.
+    :type gamma: float
+    :returns: Evaluates :math:`cov(x_t, x_s)`.
+    :rtype: float
     """
-    ans = pi * gamma**2 * erf((t - s) / (2 * gamma)) + pi * gamma**2 * erf(s / (2 * gamma))
+    gamma2 = gamma*gamma
+    gamma3 = gamma*gamma2
+
+    ans = pi * gamma2 * (s) * erf(s / (2 * gamma)) \
+        + 2 * sqrt(pi) * gamma3 * exp(-(s**2) / (4 * gamma2)) \
+        - pi * gamma2 * (t - s) * erf((t - s) / (2 * gamma)) \
+        - 2 * sqrt(pi) * gamma3 * exp(-(t - s)**2 / (4 * gamma2)) \
+        + pi * gamma2 * s * erf(t / (2 * gamma)) \
+        + 2 * sqrt(pi) * gamma3 * exp(-(t**2) / (4 * gamma2)) \
+        - 2 * sqrt(pi) * gamma3
     return ans
-    # return integrate.quad(sigma_xdot, 0, s, args=t)
+
+
