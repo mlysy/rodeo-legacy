ff4fca509e1b22358340c92b73593364bbf7c92e
Mohan Wu <mohanwu2014@gmail.com>
2019-06-03 23:29:22 -0400

    Editted test files and kernel such that analytical and numerical match

--- Tests/test_exp.py
+++ Tests/test_exp.py
@@ -1,4 +1,3 @@
-import unittest
 from scipy import integrate
 import numpy as np
 import sys
@@ -9,27 +8,27 @@ from math import exp, erf, sqrt, pi
 sys.path.insert(0, os.path.abspath('../'))
 from BayesODE.cov_fun import cov_vv_ex, cov_xv_ex, cov_xx_ex
 
+def cov_vv_ex2(t, s, gamma, alpha):
+    return exp(-abs(t-s)/gamma)/alpha
 
-class TestIntegrate(unittest.TestCase):
+def cov_xv_ex2(t, s):
+    return integrate.quad(cov_vv_ex2, 0, t, args=(s,1,1))
+
+def cov_xx_ex2(t, s):
+    return integrate.dblquad(cov_vv_ex2, 0, t, lambda s: 0, s, args=(1,1))
+
+
+def test_cov_ex(tseq):
+    N = len(tseq)
+    Sigma_an_xx = np.zeros((N,N))
+    Sigma_an_xv = np.zeros((N,N))
+
+    for i in range(N):
+        for j in range(N):
+            Sigma_an_xx[i,j] = cov_xx_ex2(tseq[i], tseq[j])[0]
+            Sigma_an_xv[i,j] = cov_xv_ex2(tseq[i], tseq[j])[0]
     
-    def cov_xv_ex2(self, t, s):
-        return integrate.quad(cov_vv_ex, 0, t, args=(s,1))
+    Sigma_nu_xx = cov_xx_ex(tseq, tseq, 1, 1)
+    Sigma_nu_xv = cov_xv_ex(tseq, tseq, 1, 1)
 
-    def cov_xx_ex2(self, t, s):
-        return integrate.dblquad(cov_vv_ex, 0, t, lambda s: 0, s, args=(1, ))
-    
-    def test_cov_xv_ex(self):
-        t = np.linspace(1/2, 1, 2)
-        for i in range(len(t)):
-            for j in range(len(t)):
-                np.testing.assert_almost_equal(cov_xv_ex(t[i],t[j],1), self.cov_xv_ex2(t[i],t[j])[0])
-
-    def test_cov_xx_ex(self):
-        t = np.linspace(1/2, 1, 2)
-        for i in range(len(t)):
-            for j in range(len(t)):
-                np.testing.assert_almost_equal(cov_xx_ex(1/2,1,1), self.cov_xx_ex2(1/2,1)[0])
-
- 
-if __name__ == '__main__':
-    unittest.main()
+    return Sigma_an_xx, Sigma_an_xv, Sigma_nu_xx, Sigma_nu_xv
--- Tests/test_square_exp.py
+++ Tests/test_square_exp.py
@@ -1,4 +1,3 @@
-import unittest
 from scipy import integrate
 import numpy as np
 import sys
@@ -9,53 +8,44 @@ from math import exp, erf, sqrt, pi
 sys.path.insert(0, os.path.abspath('../'))
 from BayesODE.cov_fun import cov_vv_se, cov_xv_se, cov_xx_se
 
+def R_se(t, s):
+    return exp(-(s - t)**2/2)
 
-class TestIntegrate(unittest.TestCase):
-    def R_se(self, t, s):
-        return exp(-(s - t)**2/2)
+def RR_se(z, t, s):
+    return R_se(t,z)*R_se(s,z)
 
-    def RR_se(self, z, t, s):
-        return self.R_se(t,z)*self.R_se(s,z)
+def Q_se(t,s):
+    return sqrt(pi/2)*(erf(s/sqrt(2)) - erf((s-t)/sqrt(2)))
 
-    def Q_se(self, t,s):
-        return sqrt(pi/2)*(erf(s/sqrt(2)) - erf((s-t)/sqrt(2)))
+def QQ_se(z, t, s):
+    return Q_se(t,z)*Q_se(s,z)
 
-    def QQ_se(self, z, t, s):
-        return self.Q_se(t,z)*self.Q_se(s,z)
+def QR_se(z, t, s):
+    return Q_se(t,z)*R_se(s,z)
 
-    def QR_se(self, z, t, s):
-        return self.Q_se(t,z)*self.R_se(s,z)
+def cov_vv_se2(t,s):
+    return integrate.quad(RR_se, -np.inf, np.inf, args=(t,s))
 
-    def cov_vv_se2(self, t,s):
-        return integrate.quad(self.RR_se, -np.inf, np.inf, args=(t,s))
+def cov_xx_se2(t,s):
+    return integrate.quad(QQ_se, -np.inf, np.inf, args=(t,s))
 
-    def cov_xx_se2(self, t,s):
-        return integrate.quad(self.QQ_se, -np.inf, np.inf, args=(t,s))
+def cov_xv_se2(t,s):
+    return integrate.quad(QR_se, -np.inf, np.inf, args=(t,s))
 
-    def cov_xv_se2(self, t,s):
-        return integrate.quad(self.QR_se, -np.inf, np.inf, args=(t,s))
+def test_cov_se(tseq):
+    N = len(tseq)
+    Sigma_an_vv = np.zeros((N,N))
+    Sigma_an_xx = np.zeros((N,N))
+    Sigma_an_xv = np.zeros((N,N))
 
-    def Q_se2(self, t, s):
-        return integrate.quad(self.R_se, 0, t, args=(s,))
+    for i in range(N):
+        for j in range(N):
+            Sigma_an_vv[i,j] = cov_vv_se2(tseq[i], tseq[j])[0]
+            Sigma_an_xx[i,j] = cov_xx_se2(tseq[i], tseq[j])[0]
+            Sigma_an_xv[i,j] = cov_xv_se2(tseq[i], tseq[j])[0]
+    
+    Sigma_nu_vv = cov_vv_se(tseq, tseq, 1, 1)
+    Sigma_nu_xx = cov_xx_se(tseq, tseq, 1, 1)
+    Sigma_nu_xv = cov_xv_se(tseq, tseq, 1, 1)
 
-    def test_Q_se(self):
-        t = np.linspace(0.1, 2, 10)
-        for i in range(len(t)):
-            for j in range(len(t)):
-                np.testing.assert_almost_equal(self.Q_se(t[i],t[j]), self.Q_se2(t[i],t[j])[0])
-
-    def test_cov_vv_se(self):
-        t = np.linspace(0.1, 2, 10)
-        for i in range(len(t)):
-            for j in range(len(t)):
-                np.testing.assert_almost_equal(cov_vv_se(t[i],t[j],1), self.cov_vv_se2(t[i],t[j])[0])
-
-
-    def test_cov_xv_se(self):
-        t = np.linspace(0.1, 2, 10)
-        for i in range(len(t)):
-            for j in range(len(t)):
-                np.testing.assert_almost_equal(cov_xv_se(t[i],t[j],1), self.cov_xv_se2(t[i],t[j])[0])
-
-if __name__ == '__main__':
-    unittest.main()
+    return Sigma_an_vv, Sigma_an_xx, Sigma_an_xv, Sigma_nu_vv, Sigma_nu_xx, Sigma_nu_xv
