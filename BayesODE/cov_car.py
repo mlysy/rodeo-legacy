d279b0b5f32a17c8cf525bb1dff79a2bac91c1b9
mohanwu <mohanwu2014@gmail.com>
2019-07-09 09:45:55 -0400

    Editted cov_car formula and added N=10 test for var_car

--- BayesODE/cov_car.py
+++ BayesODE/cov_car.py
@@ -26,7 +26,7 @@ def cov_car(tseq, roots, sigma=1, corr=False):
                 V_tilde[i,j] = Sigma_tilde[i,j] / (delta[i] + delta[j])  #V_tilde_inf
                 V_tilde[j,i] = V_tilde[i,j]
     
-        V = np.linalg.multi_dot([Q, V_tilde, Q_inv]) #Q*V_tilde_inf*Q^-1
-        cov[t] = V.dot(np.exp(Gamma*tseq[t])) #Cov_deltat
-    
+        V = np.linalg.multi_dot([Q, V_tilde, Q.T]) #Q*V_tilde_inf*Q.T
+        exp_Gamma_t = np.linalg.multi_dot([Q, np.diag(np.exp(-np.diag(D)*tseq[t])), Q_inv]) #exp(-Gamma*t) = Q*exp(-D*t)*Q^-1
+        cov[t] = V.dot(exp_Gamma_t)
     return cov
