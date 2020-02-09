9933259fc9a6379ce651109cb3222fa2a888e16c
Mohan Wu <mohanwu2014@gmail.com>
2020-02-09 10:19:17 -0500

    Updated variable names

--- probDE/Kalman/KalmanTV.py
+++ probDE/Kalman/KalmanTV.py
@@ -29,158 +29,158 @@ class KalmanTV(object):
     the state at time n given observations from k is given by :math:`\\theta_{n|K}`.
 
     Args:
-        muState_past (ndarray(nState)): Mean estimate for state at time n-1 given observations from 
+        mu_state_past (ndarray(n_state)): Mean estimate for state at time n-1 given observations from 
             times [0...n-1]; :math:`\mu_{n-1|n-1}`. 
-        varState_past (ndarray(nState, nState)): Covariance of estimate for state at time n-1 given 
+        var_state_past (ndarray(n_state, n_state)): Covariance of estimate for state at time n-1 given 
             observations from times [0...n-1]; :math:`\Sigma_{n-1|n-1}`.
-        muState_pred (ndarray(nState)): Mean estimate for state at time n given observations from 
+        mu_state_pred (ndarray(n_state)): Mean estimate for state at time n given observations from 
             times [0...n-1]; denoted by :math:`\mu_{n|n-1}`. 
-        varState_pred (ndarray(nState, nState)): Covariance of estimate for state at time n given 
+        var_state_pred (ndarray(n_state, n_state)): Covariance of estimate for state at time n given 
             observations from times [0...n-1]; denoted by :math:`\Sigma_{n|n-1}`.
-        muState_filt (ndarray(nState)): Mean estimate for state at time n given observations from 
+        mu_state_filt (ndarray(n_state)): Mean estimate for state at time n given observations from 
             times [0...n]; denoted by :math:`\mu_{n|n}`. 
-        varState_filt (ndarray(nState, nState)): Covariance of estimate for state at time n given 
+        var_state_filt (ndarray(n_state, n_state)): Covariance of estimate for state at time n given 
             observations from times [0...n]; denoted by :math:`\Sigma_{n|n}`.
-        muState_next (ndarray(nState)): Mean estimate for state at time n+1 given observations from 
+        mu_state_next (ndarray(n_state)): Mean estimate for state at time n+1 given observations from 
             times [0...N]; denoted by :math:`\mu_{n+1|N}`. 
-        varState_next (ndarray(nState, nState)): Covariance of estimate for state at time n+1 given 
+        var_state_next (ndarray(n_state, n_state)): Covariance of estimate for state at time n+1 given 
             observations from times [0...N]; denoted by :math:`\Sigma_{n+1|N}`.
-        muState (ndarray(nState)): Transition_offsets defining the solution prior; denoted by :math:`c`.
-        wgtState (ndarray(nState, nState)): Transition matrix defining the solution prior; denoted by :math:`T`.
-        varState (ndarray(nState, nState)): Variance matrix defining the solution prior; denoted by :math:`R`.
-        xMeas (ndarray(nMeas)): Measure at time n+1; denoted by :math:`y_{n+1}`.
-        muMeas (ndarray(nMeas)): Transition_offsets defining the measure prior; denoted by :math:`d`.
-        wgtMeas (ndarray(nMeas, nMeas)): Transition matrix defining the measure prior; denoted by :math:`W`.
-        varMeas (ndarray(nMeas, nMeas)): Variance matrix defining the measure prior; denoted by :math:`H`.
+        mu_state (ndarray(n_state)): Transition_offsets defining the solution prior; denoted by :math:`c`.
+        wgt_state (ndarray(n_state, n_state)): Transition matrix defining the solution prior; denoted by :math:`T`.
+        var_state (ndarray(n_state, n_state)): Variance matrix defining the solution prior; denoted by :math:`R`.
+        x_meas (ndarray(n_meas)): Measure at time n+1; denoted by :math:`y_{n+1}`.
+        mu_meas (ndarray(n_meas)): Transition_offsets defining the measure prior; denoted by :math:`d`.
+        wgt_meas (ndarray(n_meas, n_meas)): Transition matrix defining the measure prior; denoted by :math:`W`.
+        var_meas (ndarray(n_meas, n_meas)): Variance matrix defining the measure prior; denoted by :math:`H`.
 
     """
-    def __init__(self, nMeas, nState):
-        self._nMeas = nMeas
-        self._nState = nState
+    def __init__(self, n_meas, n_state):
+        self._n_meas = n_meas
+        self._n_state = n_state
     
     def predict(self, 
-                muState_past,
-                varState_past,
-                muState,
-                wgtState,
-                varState):
+                mu_state_past,
+                var_state_past,
+                mu_state,
+                wgt_state,
+                var_state):
         """
         Perform one prediction step of the Kalman filter.
         Calculates :math:`\\theta_{n|n-1}` from :math:`\\theta_{n-1|n-1}`.
         """
-        muState_pred = wgtState.dot(muState_past) + muState
-        varState_pred = np.linalg.multi_dot([wgtState, varState_past, wgtState.T]) + varState
-        return muState_pred, varState_pred
+        mu_state_pred = wgt_state.dot(mu_state_past) + mu_state
+        var_state_pred = np.linalg.multi_dot([wgt_state, var_state_past, wgt_state.T]) + var_state
+        return mu_state_pred, var_state_pred
 
     def update(self,
-               muState_pred,
-               varState_pred,
-               xMeas,
-               muMeas,
-               wgtMeas,
-               varMeas):
+               mu_state_pred,
+               var_state_pred,
+               x_meas,
+               mu_meas,
+               wgt_meas,
+               var_meas):
         """
         Perform one update step of the Kalman filter.
         Calculates :math:`\\theta_{n|n}` from :math:`\\theta_{n|n-1}`.
         """
-        muMeas_pred = wgtMeas.dot(muState_pred) + muMeas 
-        varMeasState_pred = wgtMeas.dot(varState_pred)
-        varMeasMeas_pred = np.linalg.multi_dot([wgtMeas, varState_pred, wgtMeas.T]) + varMeas
-        varStateMeas_pred = varState_pred.dot(wgtMeas.T)
-        varState_temp = solveV(varMeasMeas_pred, varStateMeas_pred.T).T
-        muState_filt = muState_pred + varState_temp.dot(xMeas - muMeas_pred)
-        varState_filt = varState_pred - varState_temp.dot(varMeasState_pred)
-        return muState_filt, varState_filt
+        mu_meas_pred = wgt_meas.dot(mu_state_pred) + mu_meas 
+        var_meas_state_pred = wgt_meas.dot(var_state_pred)
+        var_meas_meas_pred = np.linalg.multi_dot([wgt_meas, var_state_pred, wgt_meas.T]) + var_meas
+        var_state_meas_pred = var_state_pred.dot(wgt_meas.T)
+        var_state_temp = solveV(var_meas_meas_pred, var_state_meas_pred.T).T
+        mu_state_filt = mu_state_pred + var_state_temp.dot(x_meas - mu_meas_pred)
+        var_state_filt = var_state_pred - var_state_temp.dot(var_meas_state_pred)
+        return mu_state_filt, var_state_filt
         
     def filter(self,
-               muState_past,
-               varState_past,
-               muState,
-               wgtState,
-               varState,
-               xMeas,
-               muMeas,
-               wgtMeas,
-               varMeas):
+               mu_state_past,
+               var_state_past,
+               mu_state,
+               wgt_state,
+               var_state,
+               x_meas,
+               mu_meas,
+               wgt_meas,
+               var_meas):
         """
         Perform one step of the Kalman filter.
         Combines :func:`KalmanTV.predict` and :func:`KalmanTV.update` steps to get :math:`\\theta_{n|n}` from :math:`\\theta_{n-1|n-1}`.
         """
-        muState_pred, varState_pred = self.predict(muState_past = muState_past, 
-                                                   varState_past = varState_past,
-                                                   muState = muState,
-                                                   wgtState = wgtState,
-                                                   varState = varState)
-        muState_filt, varState_filt = self.update(muState_pred = muState_pred,
-                                                  varState_pred = varState_pred,
-                                                  xMeas = xMeas,
-                                                  muMeas = muMeas,
-                                                  wgtMeas = wgtMeas,
-                                                  varMeas = varMeas)
-        return muState_pred, varState_pred, muState_filt, varState_filt
+        mu_state_pred, var_state_pred = self.predict(mu_state_past = mu_state_past, 
+                                                   var_state_past = var_state_past,
+                                                   mu_state = mu_state,
+                                                   wgt_state = wgt_state,
+                                                   var_state = var_state)
+        mu_state_filt, var_state_filt = self.update(mu_state_pred = mu_state_pred,
+                                                  var_state_pred = var_state_pred,
+                                                  x_meas = x_meas,
+                                                  mu_meas = mu_meas,
+                                                  wgt_meas = wgt_meas,
+                                                  var_meas = var_meas)
+        return mu_state_pred, var_state_pred, mu_state_filt, var_state_filt
     
     def smooth_mv(self,
-                  muState_next,
-                  varState_next,
-                  muState_filt,
-                  varState_filt,
-                  muState_pred,
-                  varState_pred,
-                  wgtState):
+                  mu_state_next,
+                  var_state_next,
+                  mu_state_filt,
+                  var_state_filt,
+                  mu_state_pred,
+                  var_state_pred,
+                  wgt_state):
         """
         Perform one step of the Kalman mean/variance smoother.
         Calculates :math:`\\theta_{n|N}` from :math:`\\theta_{n+1|N}`, :math:`\\theta_{n|n}`, and :math:`\\theta_{n+1|n}`.
         """
-        varState_temp = varState_filt.dot(wgtState.T)
-        varState_temp_tilde = solveV(varState_pred, varState_temp.T).T
-        muState_smooth = muState_filt + varState_temp_tilde.dot(muState_next - muState_pred)
-        varState_smooth = varState_filt + np.linalg.multi_dot([varState_temp_tilde, (varState_next - varState_pred), varState_temp_tilde.T])
-        return muState_smooth, varState_smooth
+        var_state_temp = var_state_filt.dot(wgt_state.T)
+        var_state_temp_tilde = solveV(var_state_pred, var_state_temp.T).T
+        mu_state_smooth = mu_state_filt + var_state_temp_tilde.dot(mu_state_next - mu_state_pred)
+        var_state_smooth = var_state_filt + np.linalg.multi_dot([var_state_temp_tilde, (var_state_next - var_state_pred), var_state_temp_tilde.T])
+        return mu_state_smooth, var_state_smooth
 
     def smooth_sim(self,
-                   xState_next,
-                   muState_filt,
-                   varState_filt,
-                   muState_pred,
-                   varState_pred,
-                   wgtState):
+                   x_state_next,
+                   mu_state_filt,
+                   var_state_filt,
+                   mu_state_pred,
+                   var_state_pred,
+                   wgt_state):
         """
         Perform one step of the Kalman sampling smoother.
         Calculates a draw :math:`x_{n|N}` from :math:`x_{n+1|N}`, :math:`\\theta_{n|n}`, and :math:`\\theta_{n+1|n}`.
         """
-        varState_temp = varState_filt.dot(wgtState.T)
-        varState_temp_tilde = solveV(varState_pred, varState_temp.T).T
-        muState_sim = muState_filt + varState_temp_tilde.dot(xState_next - muState_pred)
-        varState_sim = varState_filt - varState_temp_tilde.dot(varState_temp.T)
-        xState_smooth = np.random.multivariate_normal(muState_sim, varState_sim, tol=1e-6)
-        return xState_smooth
+        var_state_temp = var_state_filt.dot(wgt_state.T)
+        var_state_temp_tilde = solveV(var_state_pred, var_state_temp.T).T
+        mu_state_sim = mu_state_filt + var_state_temp_tilde.dot(x_state_next - mu_state_pred)
+        var_state_sim = var_state_filt - var_state_temp_tilde.dot(var_state_temp.T)
+        x_state_smooth = np.random.multivariate_normal(mu_state_sim, var_state_sim, tol=1e-6)
+        return x_state_smooth
     
     def smooth(self,
-               xState_next,
-               muState_next,
-               varState_next,
-               muState_filt,
-               varState_filt,
-               muState_pred,
-               varState_pred,
-               wgtState):
+               x_state_next,
+               mu_state_next,
+               var_state_next,
+               mu_state_filt,
+               var_state_filt,
+               mu_state_pred,
+               var_state_pred,
+               wgt_state):
         """
         Perform one step of both Kalman mean/variance and sampling smoothers.
         Combines :func:`KalmanTV.smooth_mv` and :func:`KalmanTV.smooth_sim` steps to get :math:`x_{n|N}` and 
         :math:`\\theta_{n|N}` from :math:`\\theta_{n+1|N}`, :math:`\\theta_{n|n}`, and :math:`\\theta_{n+1|n}`.
         """
-        muState_smooth, varState_smooth = self.smooth_mv(muState_next = muState_next,
-                                                         varState_next = varState_next,
-                                                         muState_filt = muState_filt,
-                                                         varState_filt = varState_filt,
-                                                         muState_pred = muState_pred,
-                                                         varState_pred = varState_pred,
-                                                         wgtState = wgtState) 
-        xState_smooth = self.smooth_sim(xState_next = xState_next,
-                                        muState_filt = muState_filt,
-                                        varState_filt = varState_filt,
-                                        muState_pred = muState_pred,
-                                        varState_pred = varState_pred,
-                                        wgtState = wgtState)
-        return muState_smooth, varState_smooth, xState_smooth
+        mu_state_smooth, var_state_smooth = self.smooth_mv(mu_state_next = mu_state_next,
+                                                         var_state_next = var_state_next,
+                                                         mu_state_filt = mu_state_filt,
+                                                         var_state_filt = var_state_filt,
+                                                         mu_state_pred = mu_state_pred,
+                                                         var_state_pred = var_state_pred,
+                                                         wgt_state = wgt_state) 
+        x_state_smooth = self.smooth_sim(x_state_next = x_state_next,
+                                        mu_state_filt = mu_state_filt,
+                                        var_state_filt = var_state_filt,
+                                        mu_state_pred = mu_state_pred,
+                                        var_state_pred = var_state_pred,
+                                        wgt_state = wgt_state)
+        return mu_state_smooth, var_state_smooth, x_state_smooth
     
--- probDE/Kalman/kalman_ode_higher.py
+++ probDE/Kalman/kalman_ode_higher.py
@@ -8,7 +8,7 @@ import numpy as np
 
 from probDE.Kalman.KalmanTV import KalmanTV
 
-def kalman_ode_higher(fun, x0State, tmin, tmax, n_eval, wgtState, muState, varState, wgtMeas):
+def kalman_ode_higher(fun, x0_state, tmin, tmax, n_eval, wgt_state, mu_state, var_state, wgt_meas):
     """
     Probabilistic ODE solver based on the Kalman filter and smoother. Returns an approximate solution to the higher order ODE
 
@@ -29,89 +29,89 @@ def kalman_ode_higher(fun, x0State, tmin, tmax, n_eval, wgtState, muState, varSt
 
     Args:
         fun (function): Higher order ODE function :math:`W x_t = F(x_t, t)` taking arguments :math:`x` and :math:`t`.
-        x0State (float): Initial value of the state variable :math:`x_t` at time :math:`t = 0`.
+        x0_state (float): Initial value of the state variable :math:`x_t` at time :math:`t = 0`.
         tmin (int): First time point of the time interval to be evaluated; :math:`a`.
         tmax (int): Last time point of the time interval to be evaluated; :math:`b`.
         n_eval (int): Number of discretization points (:math:`N`) of the time interval that is evaluated, 
             such that discretization timestep is :math:`dt = b/N`.
-        wgtState (ndarray(n_dim_state, n_dim_state)): Transition matrix defining the solution prior; :math:`T`.
-        muState (ndarray(n_dim_state)): Transition_offsets defining the solution prior; :math:`c`.
-        varState (ndarray(n_dim_state, n_dim_state)): Variance matrix defining the solution prior; :math:`R`.
-        wgtMeas (ndarray(n_dim_state)): Transition matrix defining the measure prior; :math:`W`.
+        wgt_state (ndarray(n_dim_state, n_dim_state)): Transition matrix defining the solution prior; :math:`T`.
+        mu_state (ndarray(n_dim_state)): Transition_offsets defining the solution prior; :math:`c`.
+        var_state (ndarray(n_dim_state, n_dim_state)): Variance matrix defining the solution prior; :math:`R`.
+        wgt_meas (ndarray(n_dim_state)): Transition matrix defining the measure prior; :math:`W`.
         
     Returns:
         (tuple):
-        - **xStates** (ndarray(n_timesteps, n_dim_state)): Sample solution at time t given observations from times [0...N] for
+        - **x_states** (ndarray(n_timesteps, n_dim_state)): Sample solution at time t given observations from times [0...N] for
           :math:`t = 0,1/N,\ldots,1`.
-        - **muState_smooths** (ndarray(n_timesteps, n_dim_state)): Posterior mean of the solution process :math:`y_n` at times 
+        - **mu_state_smooths** (ndarray(n_timesteps, n_dim_state)): Posterior mean of the solution process :math:`y_n` at times 
           :math:`t = 0,1/N,\ldots,1`.
-        - **varState_smooths** (ndarray(n_timesteps, n_dim_state, n_dim_state)): Posterior variance of the solution process at
+        - **var_state_smooths** (ndarray(n_timesteps, n_dim_state, n_dim_state)): Posterior variance of the solution process at
           times :math:`t = 0,1/N,\ldots,1`.
 
     """
     # Dimensions of state and measure variables
-    n_dim_meas = wgtMeas.shape[0]
-    n_dim_state = len(muState)
+    n_dim_meas = wgt_meas.shape[0]
+    n_dim_state = len(mu_state)
     n_timesteps = n_eval + 1
 
     # argumgents for kalman_filter and kalman_smooth
-    muMeas = np.zeros(n_dim_meas)
-    varMeass = np.zeros((n_timesteps, n_dim_meas, n_dim_meas))
-    xMeass = np.zeros((n_timesteps,n_dim_meas))
-    muState_filts = np.zeros((n_timesteps, n_dim_state))
-    varState_filts = np.zeros((n_timesteps, n_dim_state, n_dim_state))
-    muState_preds = np.zeros((n_timesteps, n_dim_state))
-    varState_preds = np.zeros((n_timesteps, n_dim_state, n_dim_state))
-    muState_smooths = np.zeros((n_timesteps, n_dim_state))
-    varState_smooths = np.zeros((n_timesteps, n_dim_state, n_dim_state))
-    xStates = np.zeros((n_timesteps, n_dim_state))
+    mu_meas = np.zeros(n_dim_meas)
+    var_meass = np.zeros((n_timesteps, n_dim_meas, n_dim_meas))
+    x_meass = np.zeros((n_timesteps,n_dim_meas))
+    mu_state_filts = np.zeros((n_timesteps, n_dim_state))
+    var_state_filts = np.zeros((n_timesteps, n_dim_state, n_dim_state))
+    mu_state_preds = np.zeros((n_timesteps, n_dim_state))
+    var_state_preds = np.zeros((n_timesteps, n_dim_state, n_dim_state))
+    mu_state_smooths = np.zeros((n_timesteps, n_dim_state))
+    var_state_smooths = np.zeros((n_timesteps, n_dim_state, n_dim_state))
+    x_states = np.zeros((n_timesteps, n_dim_state))
 
     # initialize things
-    muState_filts[0] = x0State
-    xMeass[0] = x0State.dot(wgtMeas.T)
-    muState_preds[0] = muState_filts[0]
-    varState_preds[0] = varState_filts[0]
+    mu_state_filts[0] = x0_state
+    x_meass[0] = x0_state.dot(wgt_meas.T)
+    mu_state_preds[0] = mu_state_filts[0]
+    var_state_preds[0] = var_state_filts[0]
 
     # forward pass
     KFS = KalmanTV(n_dim_meas, n_dim_state)
     for t in range(n_eval):
-        muState_preds[t+1], varState_preds[t+1] = (
-            KFS.predict(muState_past = muState_filts[t],
-                        varState_past = varState_filts[t],
-                        muState = muState,
-                        wgtState = wgtState,
-                        varState = varState)
+        mu_state_preds[t+1], var_state_preds[t+1] = (
+            KFS.predict(mu_state_past = mu_state_filts[t],
+                        var_state_past = var_state_filts[t],
+                        mu_state = mu_state,
+                        wgt_state = wgt_state,
+                        var_state = var_state)
         )
 
-        varMeass[t+1] = np.linalg.multi_dot([wgtMeas, varState_preds[t+1], wgtMeas.T]) 
+        var_meass[t+1] = np.linalg.multi_dot([wgt_meas, var_state_preds[t+1], wgt_meas.T]) 
         I_tt = np.random.normal(loc=0.0, scale=1.0, size=n_dim_state)
-        R_tt = np.linalg.cholesky(varState_preds[t+1])
-        xState_tt = muState_preds[t+1] + R_tt.dot(I_tt) 
-        xMeass[t+1] = fun(xState_tt, tmin + (tmax-tmin)*(t+1)/n_eval)
+        R_tt = np.linalg.cholesky(var_state_preds[t+1])
+        x_state_tt = mu_state_preds[t+1] + R_tt.dot(I_tt) 
+        x_meass[t+1] = fun(x_state_tt, tmin + (tmax-tmin)*(t+1)/n_eval)
 
-        muState_filts[t+1], varState_filts[t+1] = (
-            KFS.update(muState_pred = muState_preds[t+1],
-                       varState_pred = varState_preds[t+1],
-                       xMeas = xMeass[t+1],
-                       muMeas = muMeas,
-                       wgtMeas = wgtMeas,
-                       varMeas = varMeass[t+1])
+        mu_state_filts[t+1], var_state_filts[t+1] = (
+            KFS.update(mu_state_pred = mu_state_preds[t+1],
+                       var_state_pred = var_state_preds[t+1],
+                       x_meas = x_meass[t+1],
+                       mu_meas = mu_meas,
+                       wgt_meas = wgt_meas,
+                       var_meas = var_meass[t+1])
         )
 
     # backward pass
-    muState_smooths[-1] = muState_filts[-1]
-    varState_smooths[-1] = varState_filts[-1]
-    xStates[-1] = np.random.multivariate_normal(muState_smooths[-1], varState_smooths[-1], tol=1e-6)
+    mu_state_smooths[-1] = mu_state_filts[-1]
+    var_state_smooths[-1] = var_state_filts[-1]
+    x_states[-1] = np.random.multivariate_normal(mu_state_smooths[-1], var_state_smooths[-1], tol=1e-6)
     for t in reversed(range(n_eval)):
-        muState_smooths[t], varState_smooths[t], xStates[t] = (
-            KFS.smooth(xState_next = xStates[t+1],
-                    muState_next = muState_smooths[t+1],
-                    varState_next = varState_smooths[t+1],
-                    muState_filt = muState_filts[t],
-                    varState_filt = varState_filts[t],
-                    muState_pred = muState_preds[t+1],
-                    varState_pred = varState_preds[t+1],
-                    wgtState = wgtState)
+        mu_state_smooths[t], var_state_smooths[t], x_states[t] = (
+            KFS.smooth(x_state_next = x_states[t+1],
+                    mu_state_next = mu_state_smooths[t+1],
+                    var_state_next = var_state_smooths[t+1],
+                    mu_state_filt = mu_state_filts[t],
+                    var_state_filt = var_state_filts[t],
+                    mu_state_pred = mu_state_preds[t+1],
+                    var_state_pred = var_state_preds[t+1],
+                    wgt_state = wgt_state)
         )
     
-    return xStates, muState_smooths, varState_smooths
+    return x_states, mu_state_smooths, var_state_smooths
